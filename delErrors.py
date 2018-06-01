14cd4f26037515a02ec9aaae081cc9a34fd094ca
Nikita <evenko1704@yandex.ru>
2018-06-01 05:22:13 +0500

    add scripts

---
+++ delErrors.py
@@ -0,0 +1,23 @@
+import os
+import json
+
+main_dir = 'dina_nikita'
+j = 'new_config.json'
+
+config = json.load(open(j, 'r+'))
+
+dirs = sorted([x for x in os.walk(main_dir)])
+for d in dirs[1:]:
+    num = int(d[0].split('/')[1])
+    g_pic = sorted(list(filter(lambda x: not x.startswith('.'), d[2])))
+    a = len(g_pic)
+    for c, g in enumerate(g_pic, 1):
+        if len(config[g]) != num:
+            need = list()
+            for i in range(1, 11):
+                if i not in config[g]:
+                    need.append(i)
+            print(f'{g}: num: {num}, real: {len(config[g])}, {config[g]}')
+            config[g] = need
+
+json.dump(config, open(j, 'w+'))
--- newGenFlip.py
+++ newGenFlip.py
@@ -9,18 +9,18 @@ images = list(os.walk(photo_dir))[0][2]
 good_images = g_pic = list(filter(lambda x: not x.startswith('flip') and x.endswith('.jpg'), images))
 
 def flip(name):
-    path = os.path.join(photo_dir, name)
-    image = Image.open(path).transpose(Image.FLIP_LEFT_RIGHT)
-    image.save(os.path.join(photo_dir, 'flip_' + name))
-    image.close()
+    #path = os.path.join(photo_dir, name)
+    #image = Image.open(path).transpose(Image.FLIP_LEFT_RIGHT)
+    #image.save(os.path.join(photo_dir, 'flip_' + name))
+    #image.close()
     config.update({'flip_' + name: [11 - i for i in config[name]]})
 
 
 a = len(good_images)
 for c, img in enumerate(good_images, 1):
-    if os.path.exists(os.path.join(photo_dir, 'flip_' + img)):
-        continue
+    #if os.path.exists(os.path.join(photo_dir, 'flip_' + img)):
+    #    continue
     flip(img)
     print(f'{c}/{a}')
 
-json.dump(config, open('new_config.json', 'a'))
+json.dump(config, open('new_config.json', 'w+'))
