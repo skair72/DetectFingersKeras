b4be2be131448810e81d955e4921fdca543ede57
Nikita <evenko1704@yandex.ru>
2018-06-03 18:58:10 +0500

    add unic photos

--- makeUnicPictures.py
+++ makeUnicPictures.py
@@ -24,7 +24,7 @@ temp_combinations = dict()
 l = len(good_images)
 
 
-def get_unic_from_dict(d):
+def get_unic_from_dict(d, prefix):
     unic_combinations = dict()
     for j in d:
         current_combination = old_config[j]
@@ -33,18 +33,18 @@ def get_unic_from_dict(d):
         else:
             unic_combinations.update({j: current_combination})
             copyfile(os.path.join(foler_to_parse, j), os.path.join(foler_to_save, j))
-    print(f'in {len(d)}; out {len(unic_combinations)}')
+    print(f'{prefix}: in {len(d)}; out {len(unic_combinations)}')
     return unic_combinations
 
 
 for c, i in enumerate(good_images, 1):
-    current_prefix = i[:10]
+    current_prefix = i.replace('flip', '')[:10]
     if current_prefix == prefix:
         temp_combinations.update({i: old_config[i]})
     else:
-        all_combinations.update(get_unic_from_dict(temp_combinations))
+        all_combinations.update(get_unic_from_dict(temp_combinations, prefix))
         temp_combinations.clear()
         prefix = current_prefix
 
-all_combinations.update(get_unic_from_dict(temp_combinations))
+all_combinations.update(get_unic_from_dict(temp_combinations, prefix))
 json.dump(all_combinations, open(json_to_save, 'w+'))
